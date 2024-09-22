6d20f8ce90f2117bb0d95b5d1ccc5a2b86566971
cami oliva <lamica.olivares@gmail.com>
2024-09-22 10:12:43 -0300

    add  MVC rails Picture Comment Image

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "comments/create"
+  resources :pictures
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_22_130456) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_22_131121) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,34 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_22_130456) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.bigint "picture_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["picture_id"], name: "index_comments_on_picture_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "images", force: :cascade do |t|
+    t.string "Imageable_type", null: false
+    t.bigint "Imageable_id", null: false
+    t.text "file"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["Imageable_type", "Imageable_id"], name: "index_images_on_Imageable"
+  end
+
+  create_table "pictures", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_pictures_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -58,4 +86,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_22_130456) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "comments", "pictures"
+  add_foreign_key "comments", "users"
+  add_foreign_key "pictures", "users"
 end
