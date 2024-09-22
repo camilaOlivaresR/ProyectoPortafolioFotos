abba30a589c0faf0548e4d3410fa16744fb92c64
cami oliva <lamica.olivares@gmail.com>
2024-09-22 10:29:26 -0300

    modelos

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,18 @@
 class User < ApplicationRecord
+  enum :role, [:user, :author, :admin]
+  after_initialize :set_default_role, if: :new_record?
+
+  def set_default_role
+    self.role ||= :user
+  end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :pictures, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_one_attached :avatar, dependent: :destroy
+
+  validates :name, presence: true
 end
